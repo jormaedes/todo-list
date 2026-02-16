26415ae955d10d53019009a266dc238f1a38bc0c
Jormaedes Luis <jormahed@gmail.com>
2026-02-16 13:15:47 +0100

    adicionei o metodo que renderiza os projectos listados

--- src/ui/render.js
+++ src/ui/render.js
@@ -6,7 +6,7 @@ class Render {
 		this.mainTitle = document.querySelector('#main-title');
 	}
 
-	// Este método é para me ajudar a renderizar a seção general (Inbox, Today)
+	// Renderizar a seção general (Inbox, Today)
 	renderGeneralSection() {
 		const title = this.generalSection.querySelector('.section-title');
 		this.generalSection.innerHTML = '';
@@ -31,6 +31,34 @@ class Render {
 		div.innerHTML = `
 			<i class="${item.icon}"></i>
 			${item.name}
+		`;
+		return div;
+	}
+
+	// RENDERIZAR LISTA DE PROJETOS
+	renderProjects(projects, currentProjectId) {
+		this.projectList.innerHTML = '';
+
+		projects.forEach(project => {
+			const projectElement = this._createProjectElement(project);
+
+			if (project.id === currentProjectId) {
+				projectElement.classList.add('active-project');
+			}
+			this.projectList.appendChild(projectElement);
+		});
+	}
+
+	_createProjectElement(project) {
+		const div = document.createElement('div');
+		div.classList.add('project-item');
+		div.setAttribute('data-project-id', project.id);
+
+		const taskCount = project.tasks.length;
+
+		div.innerHTML = `
+			<h3 class="project-name">${project.name}</h3>
+			<span class="qtd-project">${taskCount}</span>
 		`;
 		return div;
 	}
